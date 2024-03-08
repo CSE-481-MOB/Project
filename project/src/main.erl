b4e9d9fbc8b33fdc61b7bc198f123a6058706dd6
Ben <benmahoney2000@gmail.com>
2024-03-08 14:37:11 -0700

    More detail to Eunit tests

--- project/src/main.erl
+++ project/src/main.erl
@@ -1,15 +1,19 @@
 -module(main).
--export([request/0, deliver/0, update/0, transfer/0]).
+-export([request/0, deliver/0, update/0, transfer/1]).
 
 
 request() -> 
-  request.
+  Package1 = {"43°49'34″N", "111°47'02″W"},
+  Package1.
 
 deliver() ->
-  deliver.
+  ResponseCode = 0111010110,
+  ResponseCode.
 
 update() ->
-  update.
+  NewPackageInfo = {package_id, "Basic Package", "Singapore", "Rexburg", "10:05:56"},
+  NewPackageInfo.
 
-transfer() ->
-  transfer.
+transfer({Id, _Name, To, _From, _Timestamp}) ->
+  NewPackage = {Id, "Updated Package", To, "Boise", "10:10:34"},
+  NewPackage.
--- project/test/main_test.erl
+++ project/test/main_test.erl
@@ -3,21 +3,21 @@
 
 
 request_test() ->
-  Expected = request,
+  Expected = {"43°49'34″N", "111°47'02″W"},
   Actual = main:request(),
   ?assertEqual(Expected, Actual).
 
 deliver_test() ->
-  Expected = deliver,
+  Expected = 0111010110,
   Actual = main:deliver(),
   ?assertEqual(Expected, Actual).
 
 update_test() ->
-  Expected = update,
+  Expected = {package_id, "Basic Package", "Singapore", "Rexburg", "10:05:56"},
   Actual = main:update(),
   ?assertEqual(Expected, Actual).
 
 transfer_test() ->
-  Expected = transfer,
-  Actual = main:transfer(),
+  Expected = {0111010110, "Updated Package", "Singapore", "Boise", "10:10:34"},
+  Actual = main:transfer({0111010110, "Old Package", "Singapore", "Rexburg", "10:05:56"}),
   ?assertEqual(Expected, Actual).  
